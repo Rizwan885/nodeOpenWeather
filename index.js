f3571beeb6bd07071d98a5fcc433dd98bb715207
Rizwan <rizwanminhas7860@gmail.com>
2021-10-09 08:56:40 +0500

    Node Weather App

---
+++ index.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+var requests = require('requests');
+
+const homeFile = fs.readFileSync('home.html', 'utf-8');
+console.log(homeFile);
+const replaceVal = (tempVal, orgVal) => {
+  console.log(orgVal.main.temp);
+  let temperature = tempVal.replace('{%tempval%}', orgVal.main.temp);
+  temperature = temperature.replace('{%tempmin%}', orgVal.main.temp_min);
+  temperature = temperature.replace('{%tempmax%}', orgVal.main.temp_max);
+  temperature = temperature.replace('{%location%}', orgVal.name);
+  temperature = temperature.replace('{%country%}', orgVal.sys.country);
+  temperature = temperature.replace('{%tempstatus%}', orgVal.weather[0].main);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == '/') {
+    requests(
+      `http://api.openweathermap.org/data/2.5/weather?q=Kahuta&units=metric&appid=f4fc8c442b1d562bec491850c7a5a8fc`
+    )
+      .on('data', (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const arrData = [objdata];
+        // console.log(arrData[0].main.temp);
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join('');
+        res.write(realTimeData);
+        // console.log(realTimeData);
+      })
+      .on('end', (err) => {
+        if (err) return console.log('connection closed due to errors', err);
+        res.end();
+      });
+  } else {
+    res.end('File not found');
+  }
+});
+
+server.listen(8000, 'localhost');
